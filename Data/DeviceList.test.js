6e3ff93394c4ebe6b2a39338dcd3e88e921b2d9a
lepwn66 <61577507+lepwn66@users.noreply.github.com>
2026-03-16 23:19:54 +0100

    Update DeviceList.test.js

--- Data/DeviceList.test.js
+++ Data/DeviceList.test.js
@@ -32,7 +32,7 @@ export default {
     // "mn60o1u7xv8qh8f1": { leds: [1,2,3] },
     "stmkcsykq3kheboa": { leds: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16], name: "Battletron Light Bar" },
     "yrg6a649fkozp1hw": { leds: [1], name: "Battletron Ball Light" },
-    "keyj3w8cmutjmwk5": { leds: [1,2,3], name: "Tuya LED Strip" },
+    "keyj3w8cmutjmwk5": { leds: [1,2,3,4,5,6,7,8,9,10], name: "Tuya LED Strip" },
     // "m2jqc9ocwnxdsgs8": { leds: [1,2,3] },
     // "8cml7rj948sagivg": { leds: [1,2,3] },
     // "hy57lewrecoee6hv": { leds: [1,2,3] },
